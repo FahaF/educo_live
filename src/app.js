40c1ed629ca34387b25d6c5bcb7f32839839aeca
Faha <farihatasnimchy@gmail.com>
2021-09-20 23:07:12 +0600

    educo live

---
+++ src/app.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
+let path = require('path');
+let favicon = require('serve-favicon');
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // dirname onno jekono pc te jekono jaigai thakleo jeno html file ta pete pari sejonne, mane eta html file er location ta access kore
+});
+
+/*
+ app.post() -->for sending back data
+ mane server e data dibe. etar jonne npm install body parser lagbe
+ eta diye input data access kore hisab nikash korte parbo
+ */
+io.of('/stream').on('connection', stream);
+
+server.listen(3000);
